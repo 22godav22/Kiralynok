30ce3437080017e59ae1afa95df51561dd08ea98
GKE,Gomzi Krisztian Edvin <gomzigodav@gmail.com>
2021-02-18 10:24:23 +0100

    6.feldat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -8,6 +8,9 @@ public class Kiralynok {
         System.out.println("4 feladat: üres tábla megjelenít:");
         Tabla tabla=  new Tabla('#');
         tabla.megjelenit();
+        System.out.println("6 feladat: feltölt tábla:");
+        tabla.elhelyez(8);
+        tabla.megjelenit();
     }
     
 }
